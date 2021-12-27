e21f1dd1391a4e18503f99feee9996e4fbe95936
wulinze <824902344@qq.com>
2021-12-27 16:51:42 +0800

    add threadPool

--- net/threadPool.h
+++ net/threadPool.h
@@ -11,25 +11,87 @@
 #include <mutex>
 #include <vector>
 
-namespace threadPool{
-    template<typename F>
-    struct task{
-        static void run(){
-
-        }
-    };
-
-    class threadPool{
+namespace ThreadPool{
+    class ThreadPool{
     public:
-        threadPool(size_t n);
-        template<typename F, typename... Args> 
-        void add(task<>)
-        ~threadPool();
+        ThreadPool(size_t n);
+        template<typename F, typename... Args>
+        auto add(F&& f, Args&&... args) 
+            -> std::future<typename std::result_of<F(Args...)>::type>;
+        ~ThreadPool();
     private:
         bool isStopped = false;
         std::mutex mu_;
-        std::queue<task<>> task_queue;
+        std::queue<std::function<void>> task_queue;
         std::vector<std::thread> workers;
         std::condition_variable cv_;
     };
+
+
+    inline ThreadPool::ThreadPool(size_t n){
+        workers.reserve(n);
+
+        for(int i=0; i<n; i++){
+            workers.emplace_back(
+                [this]{
+                    for(;;){
+                        std::function<void()> task;
+
+                        {
+                            std::unique_lock<std::mutex> lock(this->mu_);
+                            cv_.wait(lock, 
+                            [this]{return this->isStopped || !this->task_queue.empty();});
+                            if(this->isStopped && this->task_queue.empty()){
+                                return;
+                            }
+
+                            task = std::move(this->task_queue.front());
+                            this->task_queue.pop();
+                        }
+
+                        task();
+                    }
+                }
+            );
+        }
+    }
+
+    template<typename F, typename... Args>
+    auto ThreadPool::add(F&& f, Args&&... args) 
+        -> std::future<typename std::result_of<F(Args...)>::type>
+    {
+        using return_type = std::result_of<F(Args...)>::type;
+
+        auto task = std::make_shared<std::packaged_task<return_type()>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        );
+
+        std::future<return_type> res = task->get_future();
+        {
+            std::unique_lock<std::mutex> lock(mu_);
+
+            if(isStopped){
+                throw std::runtime_error("ThreadPool already stopped");
+            }
+
+            task_queue.emplace([task](){(*task)();})
+        }
+        cv_.notify_one();
+
+        return res;
+    }
+
+    inline ThreadPool::~ThreadPool(){
+        {
+            std::unique_lock<std::mutex> lock(mu_);
+            isStopped = true;
+        }
+
+        cv_.notify_all();
+
+        for(auto&& worker : workers){
+            worker.join();
+        }
+    }
 }
+
